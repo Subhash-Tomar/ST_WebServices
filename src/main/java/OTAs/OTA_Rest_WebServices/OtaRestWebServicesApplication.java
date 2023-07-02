3434d8581aa4f4973281d253f36aaed433982913
subhash tomar <subhashmca07@gmail.com>
2023-07-02 22:15:49 +0530

    new

--- src/main/java/OTAs/OTA_Rest_WebServices/OtaRestWebServicesApplication.java
+++ src/main/java/OTAs/OTA_Rest_WebServices/OtaRestWebServicesApplication.java
@@ -2,13 +2,22 @@ package OTAs.OTA_Rest_WebServices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OtaRestWebServicesApplication {
+public class OtaRestWebServicesApplication extends SpringBootServletInitializer 
+{
+	
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(OtaRestWebServicesApplication.class, args);
 	}
-
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(OtaRestWebServicesApplication.class);
+    }
 }
 
